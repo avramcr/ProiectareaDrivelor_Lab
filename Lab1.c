37df8127aeed5e7e0190f7bf7d763c0bafafe363
AvramElenaCristina <128602524+avramcr@users.noreply.github.com>
2025-03-11 12:38:50 +0200

    Add files via upload

---
+++ Lab1.c
@@ -0,0 +1,51 @@
+﻿#include <windows.h>
+#include <stdio.h>
+
+
+void ListServicesImagePath() {
+    HKEY hKey;
+    TCHAR serviceName[256];
+    DWORD serviceNameSize;
+    DWORD index = 0;
+    TCHAR imagePath[1024];
+    DWORD imagePathSize;
+    DWORD type;
+
+    
+    if (RegOpenKeyEx(HKEY_LOCAL_MACHINE, TEXT("SYSTEM\\CurrentControlSet\\Services"), 0, KEY_READ, &hKey) != ERROR_SUCCESS) {
+        printf("Eroare la deschiderea registrului!\n");
+        return;
+    }
+
+   
+    while (1) {
+        serviceNameSize = sizeof(serviceName) / sizeof(serviceName[0]);
+        if (RegEnumKeyEx(hKey, index, serviceName, &serviceNameSize, NULL, NULL, NULL, NULL) != ERROR_SUCCESS) {
+            break;  
+        }
+
+       
+        TCHAR servicePath[512];
+        wsprintf(servicePath, TEXT("SYSTEM\\CurrentControlSet\\Services\\%s"), serviceName);
+
+        HKEY hServiceKey;
+        
+        if (RegOpenKeyEx(HKEY_LOCAL_MACHINE, servicePath, 0, KEY_READ, &hServiceKey) == ERROR_SUCCESS) {
+            imagePathSize = sizeof(imagePath);
+           
+            if (RegQueryValueEx(hServiceKey, TEXT("ImagePath"), NULL, &type, (LPBYTE)imagePath, &imagePathSize) == ERROR_SUCCESS) {
+                printf("%S: %S\n", serviceName, imagePath);
+            }
+            RegCloseKey(hServiceKey);
+        }
+
+        index++;
+    }
+
+    RegCloseKey(hKey);
+}
+
+int main() {
+    ListServicesImagePath();
+    return 0;
+}
